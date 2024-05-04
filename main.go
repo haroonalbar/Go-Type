e577131d7871b85fe7e4c2ddf5a0a0c84176231e
Haroon Albar <haroonalbarx@gmail.com>
2024-05-04 13:09:23 +0530

    type parameter in go

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
+// Here in Index function there is a type parameter of type T that has a constraint of comparable 
+// so it can take any comparable type like int, float, string , struct , etc
+func Index[T comparable](arr []T,item T) int{
+  for i:= 0 ; i< len(arr); i++{
+    if arr[i] == item {
+      return i
+    }
+  }
+  return -1
+}
+
 func main(){
+  //using string
+  s:=[]string{"bro","what's","up"}
+  fmt.Println(Index[string](s,"up"))
+
+  //using int
+  n:=[]int{1,34,5,334,32}
+  fmt.Println(Index[int](n,32))
+
+  //not in
+  fmt.Println(Index[int](n,0))
 
 }
