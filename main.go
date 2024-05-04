4e7a54b1dfdc1d7f5e0c27ad6fc651c4fd45d060
Haroon Albar <haroonalbarx@gmail.com>
2024-05-04 13:33:33 +0530

    generic type in go

--- main.go
+++ main.go
@@ -2,27 +2,39 @@ package main
 
 import "fmt"
 
-// Here in Index function there is a type parameter of type T that has a constraint of comparable 
-// so it can take any comparable type like int, float, string , struct , etc
-func Index[T comparable](arr []T,item T) int{
-  for i:= 0 ; i< len(arr); i++{
-    if arr[i] == item {
-      return i
-    }
-  }
-  return -1
+// Generic Type
+type List[T any] struct {
+	arr *[]T
+	val T
 }
 
-func main(){
-  //using string
-  s:=[]string{"bro","what's","up"}
-  fmt.Println(Index[string](s,"up"))
+// Here in Index function there is a type parameter of type T that has a constraint of comparable
+// so it can take any comparable type like int, float, string , struct , etc
+func Index[T comparable](arr []T, item T) int {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == item {
+			return i
+		}
+	}
+	return -1
+}
 
-  //using int
-  n:=[]int{1,34,5,334,32}
-  fmt.Println(Index[int](n,32))
+func main() {
+	//using string
+	s := []string{"bro", "what's", "up"}
+	fmt.Println(Index[string](s, "up"))
 
-  //not in
-  fmt.Println(Index[int](n,0))
+	l := List[string]{&s, "bro"}
+	fmt.Println(Index[string](*l.arr, l.val))
+
+	//using int
+	n := []int{1, 34, 5, 334, 32}
+	fmt.Println(Index[int](n, 32))
+
+	i := List[int]{&n, 5}
+	fmt.Println(Index[int](*i.arr, i.val))
+
+	//not in
+	fmt.Println(Index[int](n, 0))
 
 }
